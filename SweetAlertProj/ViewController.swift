c35da458322faf27f81fc69d192ed4b62382315e
SMSC-PMacmini <smscmacmini2@macmini2.local>
2019-01-21 11:47:34 +0530

    changes done and committed to git

---
+++ SweetAlertProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SweetAlertProj
+//
+//  Created by SMSC-PMacmini on 21/01/19.
+//  Copyright © 2019 phanindhra B V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
